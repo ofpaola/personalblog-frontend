6dd646f802f746d40eae4697896dea835ac82088
ofpaola <contatofpaola@gmail.com>
2021-04-07 14:31:35 -0300

    feat(postagem): implementando get e post

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
@@ -10,9 +10,15 @@ import { UsuarioLogin } from '../model/usuario-login';
 })
 export class AuthService {
 
+  baseUrl = environment.server + environment.port
+
   constructor(
     private http: HttpClient
   ) { }
+
+  token = {
+    headers: new HttpHeaders().set('Authorization', environment.token)
+  }
 
   entrar(usuarioLogin: UsuarioLogin): Observable<UsuarioLogin> {
     return this.http.post<UsuarioLogin>('http://localhost:8080/usuarios/logar', usuarioLogin)
@@ -20,6 +26,12 @@ export class AuthService {
 
   cadastrar(usuario: Usuario): Observable<Usuario> {
     return this.http.post<Usuario>('http://localhost:8080/usuarios/cadastrar', usuario)
+  }
+
+  getByIdUsuario(id: number): Observable<Usuario> {
+    return this.http.get<Usuario>(`${this.baseUrl}/usuarios/${id}`, {
+      headers: {'Authorization': environment.token}
+    })
   }
 
   logado() {
